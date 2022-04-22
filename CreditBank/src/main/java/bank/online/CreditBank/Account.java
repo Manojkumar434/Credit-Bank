67ab596c1d0802774d54c340646c1ad44020ee05
Manojkumar434 <manojgeetha12.10.2001@gmail.com>
2022-04-22 12:54:00 +0530

    created account

---
+++ CreditBank/src/main/java/bank/online/CreditBank/Account.java
@@ -0,0 +1,11 @@
+package bank.online.CreditBank;
+
+public class Account 
+{
+	private long accountNumber;
+	private String accountHolder;
+	private double accountBalance;
+	private long contact;
+	private String email;
+
+}
