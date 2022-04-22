397e1682d17dee38d0fde1bdd3cd7f12a30abc7c
Manojkumar434 <manojgeetha12.10.2001@gmail.com>
2022-04-22 12:40:02 +0530

    starting Point

---
+++ CreditBank/src/main/java/bank/online/CreditBank/CreditBankApplication.java
@@ -0,0 +1,13 @@
+package bank.online.CreditBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditBankApplication.class, args);
+	}
+
+}
---
+++ CreditBank/src/test/java/bank/online/CreditBank/CreditBankApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.online.CreditBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
