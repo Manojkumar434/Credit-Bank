5088b40e1bf18bb8c2b9e06345e4d4c53ecf7479
Manojkumar434 <manojgeetha12.10.2001@gmail.com>
2022-04-29 08:13:16 +0530

    TRansaction TRIED

--- CreditBank/src/main/java/bank/online/CreditBank/TransactionRepository.java
+++ CreditBank/src/main/java/bank/online/CreditBank/TransactionRepository.java
@@ -1,8 +1,18 @@
 package bank.online.CreditBank;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long>
 {
+	// findAll	>> multiple
+		// findAllByDot	>> multiple data by dot
+		// 
+		// findBy	>> single
+		// findById	>> single data by primary key
+		// findByAmount	>> single data by amount
+		
+		public List<Transaction> findAllByAccount(Account account);
 
 }
